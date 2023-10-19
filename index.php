6cf085a47ca71d8985907922ea9af90c0860cd7b
Vipendra25 <61882448+Vipendra25@users.noreply.github.com>
2023-10-19 13:44:30 +1100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <h1>This is SmartIQ running in an IFrame</h1>
 
-<iframe src="https://vipgupta-sciq.ap20-sb.smartcommunications.cloud/Produce/wizard/01fb817f-fdc5-4b5e-848c-7d9e5dc85941?portal=1" name="iframe_a" height="800px" width="100%" title="Iframe Example"></iframe>
+<iframe src="https://vipgupta-sciq.ap20-sb.smartcommunications.cloud/Produce/wizard/01fb817f-fdc5-4b5e-848c-7d9e5dc85941?portal=1" name="iframe_a" height="80%" width="100%" title="Iframe Example"></iframe>
 
 <h1>Thanks</h1>
 <?php
