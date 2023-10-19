49c5c6dee41db90be0ce38800c368998d47de657
Vipendra25 <61882448+Vipendra25@users.noreply.github.com>
2023-10-19 13:40:48 +1100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <h1>My first PHP page</h1>
 
-<iframe src="https://www.w3schools.com/" name="iframe_a" height="300px" width="100%" title="Iframe Example"></iframe>
+<iframe src="https://vipgupta-sciq.ap20-sb.smartcommunications.cloud/Produce" name="iframe_a" height="300px" width="100%" title="Iframe Example"></iframe>
 
 <?php
 echo "Hello World!";
