c8414064f3b0fb810d2e108373a97d88d6a6942a
Vipendra25 <61882448+Vipendra25@users.noreply.github.com>
2023-10-19 13:43:49 +1100

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>This is SmartIQ running in an IFrame</h1>
 
-<iframe src="https://vipgupta-sciq.ap20-sb.smartcommunications.cloud/Produce" name="iframe_a" height="300px" width="100%" title="Iframe Example"></iframe>
+<iframe src="https://vipgupta-sciq.ap20-sb.smartcommunications.cloud/Produce/wizard/01fb817f-fdc5-4b5e-848c-7d9e5dc85941?portal=1" name="iframe_a" height="800px" width="100%" title="Iframe Example"></iframe>
 
+<h1>Thanks</h1>
 <?php
 echo "Hello World!";
 ?>
